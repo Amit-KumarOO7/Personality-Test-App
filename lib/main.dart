105f1e09642d53eb98e588ed7632d5f95de8a4bf
Amit-KumarOO7 <amitkumar01122000@gmail.com>
2021-01-20 06:24:10 +0530

    widgets split

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,6 +13,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var _questions = [
+    {
+      'questionText': 'What is your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ],
+    },
+    {
+      'questionText': 'What is your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Cat', 'score': 9}
+      ],
+    },
+    {
+      'questionText': 'Who is your favorite food',
+      'answers': [
+        {'text': 'Recfzs', 'score': 1},
+        {'text': 'Gafs', 'score': 2},
+        {'text': 'Nfsfsfs', 'score': 3},
+        {'text': 'Mefsfs', 'score': 4}
+      ],
+    },
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
@@ -31,18 +62,9 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question('The question'),
-            RaisedButton(
-              child: Text('Answer1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer2'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer3'),
-              onPressed: _answerQuestion,
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
